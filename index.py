b836fb309553e3e0682f2fa240afbaa7a679d165
Isaac Felipe <62969919+isaacfelipe1@users.noreply.github.com>
2024-06-12 20:41:03 -0300

    Update index.py

--- index.py
+++ index.py
@@ -2,15 +2,13 @@ import cv2
 import os
 
 diretorio = "C:\\testesIsaac\\"
-
 for filename in os.listdir(diretorio):
     if filename.lower().endswith((".jpg", ".jpeg", ".png")):
         filepath = os.path.join(diretorio, filename)
         image = cv2.imread(filepath)
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
        
-        _, binary_image = cv2.threshold(gray_image, 128, 255, cv2.THRESH_BINARY)
-        
+        _, binary_image = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
         extensao = os.path.splitext(filename)[1]
         output_path = os.path.join(diretorio, filename.split(".")[0] + "" + extensao)
         cv2.imwrite(output_path, binary_image)
